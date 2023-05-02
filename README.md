4b431195882c9b1ea381358b05eb05850db9f01b
Ben Harrison <53424421+benhar-dev@users.noreply.github.com>
2023-05-02 12:28:18 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Simple example of creating a dialog using javascript and topmostlayer in TwinCAT HMI
+
+## Disclaimer
+
+This is a personal guide not a peer reviewed journal or a sponsored publication. We make
+no representations as to accuracy, completeness, correctness, suitability, or validity of any
+information and will not be liable for any errors, omissions, or delays in this information or any
+losses injuries, or damages arising from its display or use. All information is provided on an as
+is basis. It is the reader’s responsibility to verify their own facts.
+
+The views and opinions expressed in this guide are those of the authors and do not
+necessarily reflect the official policy or position of any other agency, organization, employer or
+company. Assumptions made in the analysis are not reflective of the position of any entity
+other than the author(s) and, since we are critically thinking human beings, these views are
+always subject to change, revision, and rethinking at any time. Please do not hold us to them
+in perpetuity.
+
+## Overview
+
+This is a small example of using the topmostlayer in the TwinCAT HMI API.
+
+## Getting started
+
+Open TwinCAT HMI and switch to liveview. Press Show Dialog to see the results. Feedback is shown on the result label
+
+## Screenshot
+
+![image](./docs/images/Screenshot.png)
+
+## Versions
+
+- TcXaeShell 3.1.4024.44
+- TwinCAT HMI 1.12.758.8
+
+## Need more help?
+
+Please visit http://beckhoff.com/ for further guides
---
+++ src/tchmi-javascript-topmostlayer-as-dialog/Scripts/ShowOkCancelDialog.js
@@ -0,0 +1,59 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.758.8/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var tchmi_usercontrol_as_dialog;
+        (function (tchmi_usercontrol_as_dialog) {
+            function ShowOkCancelDialog(ctx, title, content) {
+
+                var myPopup = $(`
+                 <div id="popup" style="background-color: #fff; padding: 20px; border-radius: 8px; max-width: 300px; text-align: left; margin: 0 auto;">
+                    <p style="font-size: 24px; font-weight: bold; margin-top: 0px; margin-bottom: 10px;">${title}</p>
+                    <p style="font-size: 16px; margin-bottom: 20px;">${content}</p>
+
+                    <div>
+                      <button id="okButton" style="border: none; padding: 8px 16px; border-radius: 4px; margin-right: 10px; cursor: pointer; font-weight: 500; background-color: #007bff; color: #fff;">OK</button>
+                      <button id="cancelButton" style="border: none; padding: 8px 16px; border-radius: 4px; cursor: pointer; font-weight: 500; background-color: #6c757d; color: #fff;">Cancel</button>
+                    </div>
+                </div>`);
+
+                let _this = this;
+
+                function ok() {
+                    ctx.success(true);
+                    TcHmi.TopMostLayer.remove(_this, myPopup);
+                }
+
+                function cancel() {                
+                    ctx.success(false);
+                    TcHmi.TopMostLayer.remove(_this, myPopup);
+                }
+            
+                TcHmi.TopMostLayer.add(this, myPopup, {
+                    centerHorizontal: true,
+                    centerVertical: true,
+                    removeCb: (data) => {
+                        if (data.canceled) {
+                            ctx.success(false);
+                        }
+                    }
+                });
+
+                $('#okButton').click(function () {
+                    ok();
+                });
+
+                $('#cancelButton').click(function () {
+                    cancel();
+                });
+
+                
+                
+            }
+            tchmi_usercontrol_as_dialog.ShowOkCancelDialog = ShowOkCancelDialog;
+        })(tchmi_usercontrol_as_dialog = Functions.tchmi_usercontrol_as_dialog || (Functions.tchmi_usercontrol_as_dialog = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('ShowOkCancelDialog', 'TcHmi.Functions.tchmi_usercontrol_as_dialog', TcHmi.Functions.tchmi_usercontrol_as_dialog.ShowOkCancelDialog);
